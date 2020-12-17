3370bb255ecd546fb8720024dcbac47c26b3ad5b
Andrew Creeves <Raptor707@gmail.com>
2020-12-17 08:44:54 -0800

    Added more aliases

--- .config/shellcfg/alias.sh
+++ .config/shellcfg/alias.sh
@@ -1,7 +1,27 @@
 # Aliases!
+
+# exa instead of ls
 alias ls='exa --color=auto'
 alias la='exa -la --color=auto'
 alias ll='exa -l --color=auto'
-alias gh='history|grep'
+alias l.='exa -d .* --color=auto'
+
+# cd dots
+alias ..='cd ..'
+alias ...='cd ../../../'
+alias ....='cd ../../../../'
+alias .....='cd ../../../../'
+alias .4='cd ../../../../'
+alias .5='cd ../../../../..'
+
+# show the path, date and time
+alias path='echo -e ${PATH//:/\\n}'
+alias now='date +"%T"'
+alias nowtime=now
+alias nowdate='date +"%d-%m-%Y"'
+
+# misc
+alias gh='history|grep -i'
 alias diff='diff --color=auto'
 alias grep='grep --color=auto'
+
